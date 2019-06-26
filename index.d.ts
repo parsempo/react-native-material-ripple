0fbb377447dc0703fb8dd7aceacfc8a3ff945d1b
Hunaid Hassan <hhunaid@gmail.com>
2019-06-27 02:09:08 +0500

    feat: Add RTL and TS support

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare module 'react-native-material-ripple' {
+    import { PureComponent } from 'react'
+    import { TouchableWithoutFeedbackProps, Animated, ViewProps } from 'react-native'
+
+    export interface RippleProps extends TouchableWithoutFeedbackProps, ViewProps {
+        rippleColor?: string,
+        rippleOpacity?: number,
+        rippleDuration?: number,
+        rippleSize?: number,
+        rippleContainerBorderRadius?: number,
+        rippleCentered?: boolean,
+        rippleSequential?: boolean,
+        rippleFades?: boolean,
+        disabled?: boolean,
+
+        onRippleAnimation?: (animation: Animated.TimingAnimationConfig, callback: () => void) => void
+    }
+
+    export default class Ripple extends PureComponent<RippleProps> {}
+}
